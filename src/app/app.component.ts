c1ac931008c9697f9b704b728ff0abf9a2ec3f0b
Esnchg <infixman@gmail.com>
2019-06-05 11:59:44 +0800

    change app to CSS from SASS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'taichung-ibike-map';
+  lat: number = 24.1580195;
+  lng: number = 120.6569436;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { AgmCoreModule } from '@agm/core';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AgmCoreModule.forRoot({
+      apiKey: '',
+      language: 'zh-TW'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
